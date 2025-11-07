cf0d1480e8c4af4fad9869eaaad2c857f6d566be
Scarlet Pan <scarletpan@qq.com>
2025-11-07 20:30:26 +0800

    project 工程初始化

---
+++ README.md
@@ -0,0 +1,14 @@
+# try-catch
+
+A simple try-catch utility for kotlin.
+
+## Usage
+
+In `settings.gradle.kts`:
+```kotlin
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
---
+++ try-catch/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("java-library")
+    alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+    jvmToolchain(11)
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    testImplementation(libs.junit)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.Scarlet-Pan"
+            artifactId = "try-catch"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["java"])
+            }
+            artifact(tasks["sourcesJar"])
+        }
+    }
+}
