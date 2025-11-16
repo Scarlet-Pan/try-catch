9a5b48a86db9f831b31b8a97568a4f14d86acdb4
Scarlet Pan <scarletpan@qq.com>
2025-11-16 19:10:58 +0800

    project 发布配置

--- try-catch/build.gradle.kts
+++ try-catch/build.gradle.kts
@@ -3,8 +3,12 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("java-library")
     alias(libs.plugins.kotlin.jvm)
-    `maven-publish`
+    alias(libs.plugins.maven.publish)
+    signing
 }
+
+group = "io.github.scarlet-pan"
+version = "1.0.0-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -25,16 +29,44 @@ dependencies {
     testImplementation(libs.junit)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.github.Scarlet-Pan"
-            artifactId = "try-catch"
-            version = "1.0.0"
+mavenPublishing {
+    publishToMavenCentral()
 
-            afterEvaluate {
-                from(components["kotlin"])
+    signAllPublications()
+
+    coordinates(group.toString(), "try-catch", version.toString())
+
+    pom {
+        name = "try-catch"
+        description = "A coroutine-friendly, type-safe try-catch-style wrapper for handling exception."
+        inceptionYear = "2025"
+        url = "https://github.com/scarlet-pan/try-catch"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/MIT"
             }
         }
+        developers {
+            developer {
+                id = "Scarlet-Pan"
+                name = "Scarlet Pan"
+                email = "scarletpan@qq.com"
+            }
+        }
+        scm {
+            url = "https://github.com/scarlet-pan/try-catch"
+            connection = "scm:git:https://github.com/scarlet-pan/try-catch.git"
+            developerConnection = "scm:git:ssh://git@github.com/scarlet-pan/try-catch.git"
+        }
     }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        providers.gradleProperty("signingInMemoryKeyId").orNull,
+        providers.gradleProperty("signingInMemoryKey").orNull,
+        providers.gradleProperty("signingInMemoryKeyPassword").orNull
+    )
+    sign(publishing.publications)
 }
