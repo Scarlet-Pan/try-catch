2ee15ef000fabc5bcb183085b295a286ae368821
Scarlet Pan <scarletpan@qq.com>
2025-11-16 20:17:18 +0800

    project 工程配置

--- try-catch/build.gradle.kts
+++ try-catch/build.gradle.kts
@@ -13,8 +13,6 @@ version = "1.0.0-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-    withSourcesJar()
-    withJavadocJar()
 }
 
 kotlin {
